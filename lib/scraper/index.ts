7708d00a7e5287ae8e08aba4686d652ef511995f
kunal7219 <kunalsingla721@gmail.com>
2024-07-28 13:40:18 +0530

    first commit

---
+++ lib/scraper/index.ts
@@ -0,0 +1,104 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import {
+  extractCategory,
+  extractCurrency,
+  extractDescription,
+  extractPrice,
+  extractReviewCount,
+  extractStars,
+} from "../utils";
+
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+  // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_e1a450b3-zone-pricewise:fq49xfw04i78 -k "https://geo.brdtest.com/mygeo.json"
+  // BrightData proxy Configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+  try {
+    // Fetch the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
+
+    // Extract the product title
+    const title = $("#productTitle").text().trim();
+    // console.log({ title });
+
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    const description = extractDescription($);
+    const stars = extractStars($(".a-icon-alt").first());
+    // console.log({
+    //   title,
+    //   currentPrice,
+    //   originalPrice,
+    //   outOfStock,
+    //   imageUrls,
+    //   currency,
+    //   discountRate,
+    //   description,
+    // });
+    // Construct data object with scraped information
+
+    const reviewsCount = extractReviewCount($("#acrCustomerReviewText"));
+
+    const data = {
+      url,
+      currency: currency || "₹",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "Electronics",
+      reviewsCount: reviewsCount,
+      stars: stars,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
+    // console.log(data);
+    return data;
+  } catch (error: any) {
+    throw new Error(`Failed to scrape product: ${error.message}`);
+  }
+}
---
+++ lib/utils.ts
@@ -0,0 +1,145 @@
+import { PriceHistoryItem, Product } from "../types/index";
+
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
+
+const THRESHOLD_PERCENTAGE = 40;
+
+export function extractPrice(...elements: any) {
+  for (const element of elements) {
+    const priceText = element.text().trim();
+
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
+
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
+    }
+  }
+
+  return "";
+}
+
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
+
+export function extractStars(element: any) {
+  // Extract and trim the text content of the first matched element
+  const starsText = element.text().trim();
+  return starsText ? starsText.split("out")[0].trim() : "";
+}
+
+export function extractDescription($: any) {
+  // these are possible elements holding description of the product
+  const selectors = [
+    ".a-unordered-list .a-list-item",
+    ".a-expander-content p",
+    // Add more selectors here if needed
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  // If no matching elements were found, return an empty string
+  return "";
+}
+
+export function extractReviewCount(element: any) {
+  // Extract the text content and trim any extra whitespace
+  const reviewCountText = element.text().trim();
+  //   console.log("Raw review count text:", reviewCountText); // Debugging output
+
+  // Extract only numeric characters and commas
+  const numericReviewCount = reviewCountText.replace(/[^\d,]/g, "");
+
+  // Remove any commas and convert to a number if needed
+  const cleanReviewCount = numericReviewCount.replace(/,/g, "");
+
+  return cleanReviewCount;
+}
+
+export function extractCategory(element: any): string {
+  // Map through all matched elements and get their text content
+  const categories: string[] = Array.from(element).map((el: any) =>
+    el.textContent.trim()
+  );
+  return categories.join(" > "); // Join categories with '>'
+}
+
+// Extracts description from two possible elements from amazon
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+
+  return averagePrice;
+}
+
+export const getEmailNotifType = (
+  scrapedProduct: Product,
+  currentProduct: Product
+) => {
+  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+
+  if (scrapedProduct.currentPrice < lowestPrice) {
+    return Notification.LOWEST_PRICE as keyof typeof Notification;
+  }
+  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+  }
+  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+    return Notification.THRESHOLD_MET as keyof typeof Notification;
+  }
+
+  return null;
+};
+
+export const formatNumber = (num: number = 0) => {
+  return num.toLocaleString(undefined, {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+};
