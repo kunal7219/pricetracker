9ccd3e28acfe77e80f08e63e2fe40f55154a20bf
kunal7219 <kunalsingla721@gmail.com>
2024-07-28 14:25:56 +0530

    done

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,9 +11,9 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-// export const maxDuration = 300; // This function can run for a maximum of 300 seconds
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
